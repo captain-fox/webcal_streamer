b551dfcc2bc0098a85259ab9f7f5e3a1b533100b
Stan Reduta <stanislaw.reduta@gmail.com>
2018-02-05 23:55:46 +0100

    Adds Schedule File model class and 'media' root to settings file for uploaded file.

--- web_client/models.py
+++ web_client/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class ScheduleFile(models.Model):
+    file = models.FileField(blank=False)
+    created = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=50, blank=True, unique=True)
