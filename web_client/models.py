f365e92d9e3dfa2bc383484e1591a805f7bce6ae
Stan Reduta <stanislaw.reduta@gmail.com>
2018-02-17 17:04:02 +0100

    Adds 'Term' and 'SwitchedDay' models.

--- web_client/models.py
+++ web_client/models.py
@@ -11,3 +11,19 @@ class ScheduleFile(models.Model):
 
     class Meta:
         ordering = ['created']
+
+
+class Term(models.Model):
+
+    title = models.TextField(blank=False)
+    term_begins = models.DateField(null=False)
+    term_ends = models.DateField(null=False)
+    holidays_begin = models.DateField(null=False)
+    holidays_end = models.DateField(null=False)
+
+
+class SwitchedDay(models.Model):
+
+    term = models.ForeignKey(Term, related_name='switched_days', on_delete=models.CASCADE)
+    replaces = models.DateField()
+    replaced_with = models.DateField()
