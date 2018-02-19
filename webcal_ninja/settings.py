2666b53c5d80921cea200c5d41d1be2015bdf8b7
Stan Reduta <stanislaw.reduta@gmail.com>
2018-02-19 22:11:40 +0100

    Adds second DB engine (idle).

--- webcal_ninja/settings.py
+++ webcal_ninja/settings.py
@@ -82,7 +82,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    # 'postgres': {
+    #     'NAME': 'postgres',
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '',
+    #     'HOST': '0.0.0.0',
+    #     'PORT': '5432',
+    # },
 }
 
 
