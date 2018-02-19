c385f90e40316938da97f058d878e6046133bde5
Stan Reduta <stanislaw.reduta@gmail.com>
2018-02-19 22:11:04 +0100

    Fixes bug causing DB to refuse uploading new files. Resets DB and migrations.

--- web_client/models.py
+++ web_client/models.py
@@ -5,7 +5,7 @@ import uuid
 
 class ScheduleFile(models.Model):
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid1(), editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
     file = models.FileField(null=False, upload_to='uploads/')
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=50, blank=True)
